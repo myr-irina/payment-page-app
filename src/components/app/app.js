9646b7350b3252d44a6f72585757cc5bd3249b18
Irina <myr-irina2@yandex.ru>
2022-09-16 18:15:26 +0300

    Disable zoom

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,7 +7,7 @@ function App() {
     'touchmove',
     function (event) {
       event = event.originalEvent || event;
-      if (event.scale > 1) {
+      if (event.scale !== undefined && event.scale !== 1) {
         event.preventDefault();
       }
     },
