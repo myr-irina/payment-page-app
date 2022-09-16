83fcd2e975f17d69f78e7246ff6bc29d09079224
Irina <myr-irina2@yandex.ru>
2022-09-16 18:09:37 +0300

    Disable zoom

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,12 +3,22 @@ import Header from '../header/header';
 import PaymentForm from '../payment-form/payment-form';
 
 function App() {
-  
+  document.addEventListener(
+    'touchmove',
+    function (event) {
+      event = event.originalEvent || event;
+      if (event.scale > 1) {
+        event.preventDefault();
+      }
+    },
+    false
+  );
+
   return (
     <div className='page'>
       <div className='page__container'>
-      <Header />
-        <div className='page__content'>   
+        <Header />
+        <div className='page__content'>
           <Profile />
           <PaymentForm />
         </div>
